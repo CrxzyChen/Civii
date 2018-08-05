6c04335b000deb46e6d041ccc8766b9744b59a3c
Crxzy <crxzy@outlook.com>
2018-08-05 09:44:36 +0900

    update

--- common/js/civii.js
+++ common/js/civii.js
@@ -1,4 +1,7 @@
-const tc = TouchController();
-tc.touchBase.on('touchMove',function(msg){
-    console.log(msg);
-});
+let player = new BiliH5Player
+player.create({
+    get_from_local: !0,
+    comment: "2",
+    image: "http://192.168.2.100/Civii/common/img/55708aca00e10a476da01cdab1be3a2276a5ee01.jpg",
+    video_url: "http://192.168.2.100:8082/%E9%87%8C%E7%95%AA/2002/(18%E7%A6%81%20%E3%82%A2%E3%83%8B%E3%83%A1)%20(%E7%84%A1%E4%BF%AE%E6%AD%A3)%20%E5%B0%91%E5%A5%B3%E5%B0%8F%E6%82%AA%E9%AD%94%E7%B3%BB%20%E8%A0%A2%E3%81%8F%E5%B0%91%E5%A5%B3%E3%81%9F%E3%81%A1%20(DVD%20640x480%20WMV9).mp4"
+})
